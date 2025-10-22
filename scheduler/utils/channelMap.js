d75a7dab5fc0f3b050697a15f98e31505bf43309
CWD273 <itscwd273@gmail.com>
2025-10-22 15:28:26 -0500

    Add new channels to CHANNEL_MAP

--- scheduler/utils/channelMap.js
+++ scheduler/utils/channelMap.js
@@ -1,4 +1,9 @@
 export const CHANNEL_MAP = {
   "cartoon-network": "cartoon-network-usa-eastern-feed",
   "boomerang": "boomerang",
+  "acc": "acc-network",
+  "amc": "amc-eastern-feed",
+  "american-heroes": "american-heroes-channel",
+  "animal-planet": "animal-planet-us-east",
+  "bbc-america": "bbc-america-east",
 };
